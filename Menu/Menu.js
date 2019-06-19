7fbd3c1f18b2a9fcd23f0180aea2ae218aaf19f2
Alexis Panyathong <alexispanyathong@gmail.com>
2019-06-19 15:30:24 -0500

    Completed the START HERE for the article.js
    I completd the first two problem (lines 6 and 8.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -8,6 +8,7 @@ const menu = document.querySelector(".menu")
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button")
 // Using your menuButton reference, add a click handler that calls toggleMenu
-btn.addEventListener("click", function(event) {
-  
+menuButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  toggleMenu();
 })
